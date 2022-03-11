dd1100c0640dd80aad681bd3b359f734c305a8db
dannysan66 <34259485+dannysan66@users.noreply.github.com>
2022-03-10 20:40:03 -0800

    Add files via upload

--- resources/views/todos.blade.php
+++ resources/views/todos.blade.php
@@ -39,7 +39,14 @@
       </thead>
       <tbody>
 
-
+        @foreach($todos AS $todo)
+                <tr>
+                  <td>{{ $todo->id }}</td>
+                  <td>{{ $todo->title }}</td>
+                  <td><div class="progress progress-xs"><div class="progress-bar progress-bar-danger" style="width: {{ $todo->progress }}%"></div></div></td>
+                  <td><a class="btn btn-default btn-sm" href="{{ route('todos.show',['todo'=>$todo->id]) }}">View</a></td>
+                </tr>
+                @endforeach
       <tr role="row" class="odd">
           <td class="sorting_1">1.</td>
           <td>Update software</td>
@@ -97,6 +104,7 @@
 </div>
         </div>
     </div>
+    <a href="{{ route('todos.create') }} " class="btn btn-primary" >Create</a>
 @stop
 
 @section('js')
