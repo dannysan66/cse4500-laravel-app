8135702bfc72238100eee4be8c4ad71d74c5d1fd
dannysan66 <34259485+dannysan66@users.noreply.github.com>
2022-03-11 20:25:04 -0800

    Add files via upload

--- app/Models/Calendar.php
+++ app/Models/Calendar.php
@@ -8,5 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Calendar extends Model
 {
     use HasFactory;
-    protected $fillable = ['title','start_at', 'end_at'];
+    protected $fillable = ['title','startTime', 'endTime'];
+    protected $table = 'calendar';
+    use HasFactory;
 }
