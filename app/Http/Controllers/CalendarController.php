b7b64ecaa8038985286f9a6031b2a0e6215a6742
dannysan66 <34259485+dannysan66@users.noreply.github.com>
2022-03-11 20:23:51 -0800

    Update and rename FullCalendarController.php to CalendarController.php

---
+++ app/Http/Controllers/CalendarController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CalendarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      $events = Calendar::select('title','startTime AS start','endTime AS end')->get();
+      return json_encode( compact('events')['events']);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('eventsfeed.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+          'title' => 'required',
+          'start_at' => 'required',
+          'end_at' => 'required',
+        ]);
+
+        $eventsfeed = Calendar::create([
+          'title' => $request->title,
+          'startTime' => date($request->start_at),
+          'endTime' => date($request->end_at),
+        ]);
+
+        return redirect('/calendar');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $eventsfeed = Calendar::find($id);
+        return view('eventsfeed.show',compact('calendar'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
