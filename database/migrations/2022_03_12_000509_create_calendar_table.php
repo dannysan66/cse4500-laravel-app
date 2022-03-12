cf57fb60cec7a0e339bf18347d7977049135d25d
dannysan66 <34259485+dannysan66@users.noreply.github.com>
2022-03-11 20:51:07 -0800

    Update 2022_03_12_000509_create_calendar_table.php

--- database/migrations/2022_03_12_000509_create_calendar_table.php
+++ database/migrations/2022_03_12_000509_create_calendar_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
         Schema::create('events', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->date('start_at');
-            $table->date('end_at');
+            $table->date('startTime');
+            $table->date('endTime');
              $table->timestamps();
         });
     }
